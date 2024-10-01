77a52c7b7d9bea82b079e6390ee6405f98f41fe3
muhammed aman ma <112202255+Mohdamanma@users.noreply.github.com>
2024-10-01 22:00:34 +0530

    BreadCrum added in Product Detail Page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,17 +2,19 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import BreadCrum from '../Component/BreadCrum'
+import ProductDisplay from '../Component/ProductDisplay'
 
 function Product() {
-  const {all_product }= useContext(ShopContext)
+  const { all_product } = useContext(ShopContext)
 
-  const {productId} = useParams()
-  const product = all_product.find((e) => e.id ===  Number(productId))
+  const { productId } = useParams()
+  const product = all_product.find((e) => e.id === Number(productId))
 
   // console.log("prodect id is :",productId)
   return (
-    <div>
-      <BreadCrum product={product}/>
+    <div className='h-screen'>
+      <BreadCrum product={product} />
+      <ProductDisplay product={product} />
     </div>
   )
 }
